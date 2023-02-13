fa82d440839dbddb2064fea9c8b96aa6f3722bfd
gh-actions <daniel@dataeng.ai>
2023-02-13 18:03:03 +0000

    Deploy website - based on 366f5a3fb1966d7b7d20f598daa52e6f54128f30

--- assets/js/814f3328.6d0dcb89.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[2535],{5641:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"SQRL: Updating SQL for Developers","permalink":"/blog/sql-for-developers"},{"title":"Hello, World!","permalink":"/blog/welcome"}]}')}}]);
