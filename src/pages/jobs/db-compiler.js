1751cb0c2f6e84706bab07556a0d93a6cea3964e
Matthias Broecheler <mbroecheler@users.noreply.github.com>
2023-07-28 11:01:52 -0700

    updated job description to make it more senior

--- src/pages/jobs/db-compiler.js
+++ src/pages/jobs/db-compiler.js
@@ -37,7 +37,11 @@ export default function JobPosting() {
                   At DataSQRL, we are building <Link to="https://github.com/DataSQRL/sqrl">innovative open-source technology</Link> that
                   empowers people to build streaming data and realtime AI applications.
                   We need your help to improve the core compiler component of DataSQRL which plans, optimizes, and translates
-                  the high-level SQL language into executables for each engine. You'll be working on innovative technology within
+                  the high-level SQL language into executables for each engine.
+                </p>
+                <p>
+                  As a founding engineer, you have the opportunity to build an engineering powerhouse and set the
+                  technical direction for DataSQRL while working on innovative technology within
                   an open-source community and supported by domain experts.
                 </p>
 
@@ -63,6 +67,7 @@ export default function JobPosting() {
                 <ul>
                   <li>Analytical Thinking: Ability to dissect complex problems and identify the optimal solution path.</li>
                   <li>Attention to Detail: Every line of code matters. Ensuring the precision and flawless functionality of compilers is paramount.</li>
+                  <li>Mentorship: Provides direction to junior engineers and fosters a safe environment for growth.</li>
                   <li>Collaboration: Values the input of everyone on the team. Strives to have others feel safe and heard.</li>
                   <li>Adaptability: Thrive in a dynamic environment with ever-evolving challenges and tech updates.</li>
                   <li>Simplicity: Prioritize straightforward approaches, even in intricate systems.</li>
@@ -71,8 +76,8 @@ export default function JobPosting() {
 
                 <p>
                   Sounds interesting? Use the form below to apply! We'd love for you to join our team. See how you
-                  can <Link to="/careers">supercharge your career</Link> at DataSQRL by growing your skills with the mentorship of
-                  technical experts and experienced entrepreneurs in a safe, supportive environment.
+                  can <Link to="/careers">supercharge your career</Link> at DataSQRL by growing your skills and growing a team
+                  of committed engineers in a safe, supportive environment.
                 </p>
 
                 <p>
